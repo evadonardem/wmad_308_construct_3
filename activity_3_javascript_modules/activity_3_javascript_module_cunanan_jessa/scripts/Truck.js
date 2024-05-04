79546076d4cb12bc94605cf35641cc9dbd136c4b
Chetlie Dawan <chetlied@gmail.com>
2024-05-04 13:35:39 +0800

    activity_3_javascript_module_cunanan_jessa

---
+++ activity_3_javascript_modules/activity_3_javascript_module_cunanan_jessa/scripts/Truck.js
@@ -0,0 +1,15 @@
+export const Trucks = new Map();
+export function create(runtime, count)
+{
+ for (let i = 0; i<5; i++){
+ const newTruck = runtime.objects.
+ 	Truck.createInstance(0,100,100);	
+	
+	newTruck.width = newTruck.width * 0.25;
+	newTruck.height = newTruck.height * 0.25;
+	newTruck.angle = 90;
+	
+	newTruck.x=i * newTruck.width;
+	
+ }
+}
